6bfaa5905bd7aa7e243dad9d7596b0b9f3fca345
shribliss <34868256+shribliss@users.noreply.github.com>
2018-01-13 10:36:21 +0530

    Atom Beautify

--- js/backToTopFunction.js
+++ js/backToTopFunction.js
@@ -1,24 +1,24 @@
-function backToTop(){
-// Back to Top
-// hide #back-top first
-$("#back-top").hide();
-// fade in #back-top
-$(function () {
-  $(window).scroll(function () {
-    if ($(this).scrollTop() > 300) {
-      $('#back-top').fadeIn();
-    } else {
-      $('#back-top').fadeOut();
-    }
-  });
+function backToTop() {
+  // Back to Top
+  // hide #back-top first
+  $("#back-top").hide();
+  // fade in #back-top
+  $(function() {
+    $(window).scroll(function() {
+      if ($(this).scrollTop() > 300) {
+        $('#back-top').fadeIn();
+      } else {
+        $('#back-top').fadeOut();
+      }
+    });
 
-  // scroll body to 0px on click
-  $('#back-top').click(function () {
-    $('body,html').animate({
-      scrollTop: 0
-    }, 1200);
-    return false;
+    // scroll body to 0px on click
+    $('#back-top').click(function() {
+      $('body,html').animate({
+        scrollTop: 0
+      }, 1200);
+      return false;
+    });
   });
-});
 
 }
